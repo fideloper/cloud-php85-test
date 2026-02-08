8de52d96b8c6b18f0ff4c5196ccbc49779187256
Chris Fidao <fideloper@gmail.com>
2026-02-08 15:39:25 -0500

    some things to test caching with

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/json', function () {
+    return response()->json(['foo' => 'bar'])
+        ->header('Cache-Control', 'public, max-age=60, s-maxage=300');
+})->withoutMiddleware([
+    \Illuminate\Cookie\Middleware\EncryptCookies::class,
+    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+]);
