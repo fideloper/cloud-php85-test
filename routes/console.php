8f757d77b7e28c2f4f9919a63b5e86ee22764185
Chris Fidao <fideloper@gmail.com>
2026-02-26 15:03:57 -0500

    push some bandwidth

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    try {
+        Http::attach('image', file_get_contents(public_path('image.png')), 'image.png')
+            ->post('https://google.com');
+    } catch (\Throwable) {
+        // Ignore failures
+    }
+})->everyMinute();
